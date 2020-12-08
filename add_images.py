455ca76a40d362f1aa46036fb9dbd194c6a4f6e9
stephdef08 <stephdef08@hotmail.com>
2020-12-08 14:43:22 +0100

    feature extraction on GPU

--- add_images.py
+++ add_images.py
@@ -1,8 +1,10 @@
 import redis
 import os
 from argparse import ArgumentParser, ArgumentTypeError
+from PIL import Image
+from torchvision import transforms
+import torch
 import densenet
-import cv2
 
 if __name__ == "__main__":
     usage = "python3 add_images.py --path <folder> [--extractor <algorithm>]"
@@ -26,10 +28,14 @@ if __name__ == "__main__":
         print(usage)
         exit()
 
+    if args.path[-1] != '/':
+        print("The path mentionned is not a folder")
+        exit()
+        
     model = None
 
     if args.extractor == 'densenet':
-        model = densenet.model()
+        model = densenet.Model()
 
     if model is None:
         print("Unkown feature extractor")
@@ -37,12 +43,18 @@ if __name__ == "__main__":
 
     r = redis.Redis(host='localhost', port='6379', db=0)
 
+    transform = transforms.Compose([
+        transforms.Resize(224),
+        transforms.ToTensor(),
+        transforms.Normalize(
+            mean=[0.485, 0.456, 0.406],
+            std=[0.229, 0.224, 0.225]
+        )
+    ])
+
     for subdir, dirs, files in os.walk(args.path):
         for file in files:
-            img = cv2.imread(os.path.join(subdir, file), cv2.IMREAD_COLOR)
-            img_resized = cv2.resize(img, (224, 224))
-            img_resized_planar = np.zeros((3, 224, 224))
-            for i in range(3):
-                img_resized_planar[i, :, :] = img_resized[:, :, i]
-
-            img_tensor = torch.from_numpy(img_resized_planar)
+            img = Image.open(os.path.join(subdir, file)).convert('RGB')
+            img_t = transform(img).cuda()
+            batch = torch.unsqueeze(img_t, 0)
+            out = model(batch)
--- densenet.py
+++ densenet.py
@@ -1,4 +1,12 @@
 import torchvision.models as models
+import torch
+import torch.nn as nn
 
-def model():
-    return models.densenet161(pretrained=True)
+class Model(nn.Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.model = models.densenet161(pretrained=True).cuda()
+        self.model.eval()
+
+    def forward(self, input):
+        return self.model(input)
